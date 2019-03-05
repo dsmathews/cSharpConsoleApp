68fdac5732502aeb64a1a90996761347c1db6edb
Darrell Mathews <smathewsbd@gmail.com>
2019-03-05 11:28:54 -0500

    initial push

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace sarcasm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your quest?");
+            Console.ReadLine();
+
+            Console.WriteLine("What is the average air-speed velocity of an unladen swallow?");
+            string swallow = Console.ReadLine();
+            string answer1 = "African or European";
+            string answer2 = "african or european";
+            if (answer1 == swallow) {
+                Console.WriteLine("Well... Off you go!");
+            }
+            else if (answer2 == swallow) {
+                Console.WriteLine("Well... Off you go!");
+            }
+            else{
+                Console.WriteLine("AAAAAAAAAAAHHHHHHHH");
+            }
+
+
+        }
+    }
+}
